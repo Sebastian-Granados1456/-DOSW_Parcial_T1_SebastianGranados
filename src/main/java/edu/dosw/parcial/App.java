313b559fffd8e515ffb5d4b1eed4ce52c3726906
Seb1456 <kamilotegranados@gmail.com>
2026-09-01 19:56:03 -0500

    feat: Parcial parte 2 - Últimos retoques

--- src/main/java/edu/dosw/parcial/App.java
+++ src/main/java/edu/dosw/parcial/App.java
@@ -1,5 +1,7 @@
 package edu.dosw.parcial;
 
+import edu.dosw.parcial.pedido.GestorPedidos;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        GestorPedidos.ejecutar();
     }
 }
